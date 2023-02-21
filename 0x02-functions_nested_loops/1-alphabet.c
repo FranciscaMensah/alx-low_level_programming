b0f0bd4af5f98d812cc42c71f2590aec59c1ad4c
FranciscaMensah <franciscamensah181@gmail.com>
2023-02-21 02:45:37 -0800

    My newest commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - prints a character to
+ * the standard output
+ */
+void print_alphabet(void)
+{
+	printf("abcdefghijklmnopqrstuvwxyz\n");
+}
+/**
+ * main - check the code
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
